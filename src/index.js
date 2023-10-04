727a377ec2735e5212bf771ead1d45c097c8cde1
Zhen Luo <zhenluo1988@gmail.com>
2023-10-04 10:13:30 +0900

    rendered the first flat by using flat component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Hello from './components/hello';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello name="Boris"/>
+    <App />
   </React.StrictMode>
 );
 
